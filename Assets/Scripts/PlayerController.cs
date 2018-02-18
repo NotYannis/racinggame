f13c75064fa4f79f87958df10b2bf2fe900a1e04
Yannis Attard <yannis.attard@gmail.com>
2018-02-18 22:10:12 +0100

    Added steering wheel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TouchScript;
 
 public class PlayerController : MonoBehaviour {
-    public Vector2 acceleration;
-    public Vector2 maxVelocity;
-    public Vector2 brakeSpeed;
-    public Rigidbody2D rb;
-    public bool rotate;
+    public float acceleration;
+    public float maxVelocity;
+    public float brakeSpeed;
+    private Rigidbody2D rb;
 
-
+    float xScreenCenter;
+    GameObject steeringWheel;
+    float steeringWheelBaseRotation;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if(rb.velocity.sqrMagnitude < maxVelocity.sqrMagnitude)
+        xScreenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.0f, 0.0f)).x;
+        steeringWheel = GameObject.Find("SteeringWheel");
+        steeringWheelBaseRotation = steeringWheel.transform.localEulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+		if(rb.velocity.sqrMagnitude < maxVelocity)
         {
-            rb.AddForce(acceleration, ForceMode2D.Force);
+            rb.AddForce(acceleration * transform.TransformDirection(Vector2.right), ForceMode2D.Force);
         }
-	}
+        float rotate = steeringWheel.transform.localEulerAngles.z - steeringWheelBaseRotation;
+        rb.AddTorque(rotate * brakeSpeed);
 
-
+    }
 }
