8492dec3a06fd5426a49392fa84e1c36c5fc6dfa
Yannis Attard <yannis.attard@gmail.com>
2018-02-18 14:53:54 +0100

    Added 2d, 3D assets and touchScripts plugin

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Vector2 acceleration;
+    public Vector2 maxVelocity;
+    public Vector2 brakeSpeed;
+    public Rigidbody2D rb;
+    public bool rotate;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(rb.velocity.sqrMagnitude < maxVelocity.sqrMagnitude)
+        {
+            rb.AddForce(acceleration, ForceMode2D.Force);
+        }
+	}
+
+
+}
