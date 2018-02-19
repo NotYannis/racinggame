89eb3121182748cf0aba341bb458c716d584cce0
Yannis Attard <yannis.attard@gmail.com>
2018-02-19 22:34:20 +0100

    Add degub menu and screen scaler

---
+++ Assets/Scripts/DebugMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugMenu : MonoBehaviour {
+    Text speed;
+    Text targetRotation;
+    Text currentRotation;
+
+
+	// Use this for initialization
+	void Start () {
+        speed = GameObject.Find("Speed").GetComponent<Text>();
+        targetRotation = GameObject.Find("TargetRotation").GetComponent<Text>();
+        currentRotation = GameObject.Find("CurrentRotation").GetComponent<Text>();
+    }
+	
+	public void SetSpeedText(float speedValue)
+    {
+        speed.text = "Speed : " + speedValue.ToString();
+    }
+
+    public void SetTargetRotationText(float targetRotationValue)
+    {
+        targetRotation.text = "TargetRotation : " + targetRotationValue.ToString();
+    }
+
+    public void SetCurrentRotationText(float currentRotationValue)
+    {
+        currentRotation.text = "CurrentRotation : " + currentRotationValue.ToString();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,19 @@ public class PlayerController : MonoBehaviour {
     public float maxVelocity;
     public float brakeSpeed;
     private Rigidbody2D rb;
+    public DebugMenu debug;
 
     float xScreenCenter;
     GameObject steeringWheel;
     float steeringWheelBaseRotation;
+
 	// Use this for initialization
 	void Awake () {
         rb = GetComponent<Rigidbody2D>();
         xScreenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.0f, 0.0f)).x;
         steeringWheel = GameObject.Find("SteeringWheel");
         steeringWheelBaseRotation = steeringWheel.transform.localEulerAngles.z;
+        debug = GameObject.Find("DebugCanvas").GetComponent<DebugMenu>();
     }
 
     // Update is called once per frame
@@ -26,8 +29,14 @@ public class PlayerController : MonoBehaviour {
         {
             rb.AddForce(acceleration * transform.TransformDirection(Vector2.right), ForceMode2D.Force);
         }
-        float rotate = steeringWheel.transform.localEulerAngles.z - steeringWheelBaseRotation;
+        float rotate = steeringWheel.transform.localEulerAngles.z;
+        debug.SetTargetRotationText(rotate * brakeSpeed);
         rb.AddTorque(rotate * brakeSpeed);
+        debug.SetSpeedText(rb.velocity.magnitude);
+        debug.SetCurrentRotationText(rb.rotation);
+    }
 
+    private void LateUpdate()
+    {
     }
 }
